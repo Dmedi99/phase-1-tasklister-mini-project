5b3e7ced8d832ba5731cb6bd058a9b825828d8a1
David <davidmedina0907@gmail.com>
2023-07-01 01:23:49 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('create-task-form').addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const inputValue = document.getElementById('new-task-description').value
+    const task = document.createElement('li')
+    task.textContent = inputValue
+
+    document.getElementById('tasks').appendChild(task)
+  })
 });
